74a8452ad547c61161c0435e320419c42c597871
Ng Kok Sheng <118619477+Ng-Kok-Sheng@users.noreply.github.com>
2023-06-15 16:20:09 +0800

    Component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
-import styles from "./style"
+import styles from "./style";
+import {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Client,
+  CTA,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
+
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          NavBar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Client />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
