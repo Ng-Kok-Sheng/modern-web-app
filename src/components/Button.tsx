1bc58bcb94522709a4b1fbd6c068c742190d6c57
Ng Kok Sheng <118619477+Ng-Kok-Sheng@users.noreply.github.com>
2023-06-16 00:42:36 +0800

    Button Component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+interface IButton {
+  styles: string;
 }
 
-export default Button
+const Button = ({ styles }: IButton) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
